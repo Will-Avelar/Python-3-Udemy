401ad6bc835ea3c32e7175b673865da054bee2b1
Will-Avelar <willavelar@icloud.com>
2022-12-18 23:00:45 -0300

    12

--- aprendendo_python.py
+++ aprendendo_python.py
@@ -1,3 +1,4 @@
+
 print('Hello World')
 
 """
@@ -5,10 +6,9 @@ DocString
 E escrever o que eu
 quiser
 asdfasdfd
-"""
+
 
 ''' Usar para escrever suas notas '''
-
 # Permite escrever um comentário
 print(123)  # Na frente
 # Abaixo
@@ -19,13 +19,14 @@ print(12, 34, 1011, sep="....")
 print(56, 78, sep='-', end='\n')
 print(9, 10, sep='-', end='\n')
 
-"""
+
 DocString
 Python = Linguagem de programação
 Tipo de tipagem = Dinâmica / Forte
 str -> string -> texto
 Strings são textos que estão dentro de aspas
 """
+'''
 print(1234)
 
 # Aspas simples
@@ -189,3 +190,126 @@ print(linha_3)
 # Luiz Otávio tem 1.80 de altura,
 # pesa 95 quilos e seu IMC é
 # 29.320987654320987
+
+a = 'AAAAA'
+b = 'BBBBBB'
+c = 1.1
+string = 'b={nome2} a={nome1} a={nome1} c={nome3:.2f}'
+formato = string.format(
+    nome1=a, nome2=b, nome3=c
+)
+
+print(formato)
+
+# nome = input('Qual o seu nome? ')
+# print(f'O seu nome é {nome}')
+
+numero_1 = input('Digite um número: ')
+numero_2 = input('Digite outro número: ')
+
+int_numero_1 = int(numero_1)
+int_numero_2 = int(numero_2)
+
+print(f'A soma dos números é: {int_numero_1 + int_numero_2}')
+
+
+nome = input ('Qual é o seu nome? ')
+print(f'O seu nome é {nome}')
+
+# if / elif      / else
+# se / se não se / se não
+entrada = input('Você quer "entrar" ou "sair"? ')
+
+if entrada == 'entrar':
+    print('Você entrou no sistema')
+
+    print(12341234)
+elif entrada == 'sair':
+    print('Você saiu do sistema')
+else:
+    print('Você não digitou nem entrar e nem sair.')
+
+print('FORA DOS BLOCOS')
+
+
+# if / elif      / else
+# se / se não se / se não
+
+condicao1 = True
+condicao2 = True
+condicao3 = True
+condicao4 = True
+
+if condicao1:
+    print('Código para condição 1')
+    print('Código para condição 1')
+elif condicao2:
+    print('Código para condição 2')
+elif condicao3:
+    print('Código para condição 3')
+elif condicao4:
+    print('Código para condição 4')
+else:
+    print('Nenhuma condição foi satisfeita.')
+
+if 10 == 10:
+    print('Outro if')
+
+print('Fora do if')
+
+"""
+Operadores de comparação (relacionais)
+OP      Significado         Exemplo (True)
+>       maior               2 > 1
+>=      maior ou igual      2 >= 2
+<       menor               1 < 2
+<=      menor ou igual      2 <= 2
+==      igual               'a' == 'a'
+!=      diferente           'a' != 'b'
+"""
+maior = 2 > 1
+maior_ou_igual = 2 >= 2
+menor = 1 < 2
+menor_ou_igual = 2 <= 2
+igual = 'a' == 'a'
+diferente = 'a' != 'b'
+print('Olha meu print aqui')
+'''
+'''
+primeiro_valor = input('digite um valor: ')
+segundo_valor = input('digite outro valor: ')
+
+if primeiro_valor > segundo_valor:
+    print(
+        f'{primeiro_valor=} é maior'
+        f' do que {primeiro_valor=}'
+    
+    )
+    '''
+    
+    # Operadores lógicos
+# and (e) or (ou) not (não)
+# and - Todas as condições precisam ser
+# verdadeiras.
+# Se qualquer valor for considerado falso,
+# a expressão inteira será avaliada naquele valor
+# São considerados falsy (que vc já viu)
+# 0 0.0 '' False
+# Também existe o tipo None que é
+# usado para representar um não valor
+# entrada = input('[E]ntrar [S]air: ')
+# senha_digitada = input('Senha: ')
+
+# senha_permitida = '123456'
+
+# if entrada == 'E' and senha_digitada == senha_permitida:
+#     print('Entrar')
+# else:
+#     print('Sair')
+
+# Avaliação de curto circuito
+'''
+print(True and False and True)
+print(True and 0 and True)
+'''
+print(True and True and True) 
